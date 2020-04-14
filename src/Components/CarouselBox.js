b3d4ca157d70f0b01f4acebfc2319dfefe79dc0f
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-04-14 23:19:17 +0300

    add slader for home page

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import first from '../asserts/first.jpeg';
+import second from '../asserts/second.jpeg';
+
+export default class CarouselBox extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={ first }
+                        alt="first"
+                    />
+                    <Carousel.Caption>
+                        <h3>First image</h3>
+                        <p>Some text number one</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={ second }
+                        alt="second"
+                    />
+                    <Carousel.Caption>
+                        <h3>Second image</h3>
+                        <p>Some text number two</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
